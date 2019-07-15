d6e5e411e06193c519bb77ceff0cee4f3498443d
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-15 21:38:47 +0200

    Add client side queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// import components
+import QuestionList from './components/BookList';
+import AddQuestion from './components/AddBook';
+
+// apollo client setup
+const client = new ApolloClient({
+    uri: 'http://localhost:40000/graphql'
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div id="main">
+                <h1>Interview Questions:</h1>
+                <QuestionList />
+                <AddQuestion />
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
