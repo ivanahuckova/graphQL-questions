4800ea85aef12af0507c61567cc912d0f076e813
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-15 21:13:21 +0200

    Add mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -32,3 +32,74 @@ const AreaType = new GraphQLObjectType({
         }
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        question: {
+            type: QuestionType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Question.findById(args.id);
+            }
+        },
+        area: {
+            type: AreaType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Area.findById(args.id);
+            }
+        },
+        questions: {
+            type: new GraphQLList(QuestionType),
+            resolve(parent, args) {
+                return Question.find();
+            }
+        },
+        areas: {
+            type: new GraphQLList(AreaType),
+            resolve(parent, args) {
+                return Area.find();
+            }
+        }
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addArea: {
+            type: AreaType,
+            args: {
+                areaName: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                let area = new Area({
+                    name: args.name
+                });
+                return area.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                question: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: new GraphQLNonNull(GraphQLString) },
+                areaId: { type: new GraphQLNonNull(GraphQLID) }
+            },
+            resolve(parent, args) {
+                let question = new Question({
+                    question: args.question,
+                    description: args.description,
+                    areaId: args.areaId
+                });
+                return question.save();
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
